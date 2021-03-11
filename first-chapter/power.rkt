d15d015d88ba2085bdf325e7cec72407fd875e67
m3xan1k <dev.serzh@gmail.com>
2021-03-11 16:03:59 +0300

    fast-power

--- first-chapter/power.rkt
+++ first-chapter/power.rkt
@@ -1,9 +1,20 @@
 #lang racket
 
+(define (even? num)
+  (equal? 0 (remainder num 2)))
+
+(define (square num)
+  (* num num))
+
+(define (power number degree)
+  (power-iter number degree 1))
+
 (define (power-iter number degree acc)
-  (if (equal? degree 1) 
+  (if (equal? degree 0) 
       acc
       (power-iter number (- degree 1) (* acc number))))
 
-(define (power number degree)
-  (power-iter number degree number))
+(define (fast-power number degree) 
+  (cond ((equal? degree 0) 1)
+        ((even? degree) (square (fast-power number (/ degree 2))))
+        (else (* number (fast-power number (- degree 1)))))) 
