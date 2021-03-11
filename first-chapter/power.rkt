1533abdf7d1a1ff675964cf3d9889b728ad0ddaa
m3xan1k <dev.serzh@gmail.com>
2021-03-11 17:14:17 +0300

    double half

--- first-chapter/power.rkt
+++ first-chapter/power.rkt
@@ -18,3 +18,19 @@
   (cond ((equal? degree 0) 1)
         ((even? degree) (square (fast-power number (/ degree 2))))
         (else (* number (fast-power number (- degree 1)))))) 
+
+(define (double-iter num count)
+    (if (= count 0)
+        num
+        (double-iter (+ num 1) (- count 1))))
+
+(define (double num)
+  (double-iter num num))
+
+(define (half-iter num acc)
+  (if (= acc (/ num 2))
+      acc
+      (half-iter num (- acc 1))))
+
+(define (half num)
+  (half-iter num num))
